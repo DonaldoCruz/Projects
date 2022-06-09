17bd390986241e2cf42c47c0308549faa20daab3
Donaldo Cruz <donaldoacm@gmail.com>
2022-06-09 16:06:47 -0400

    Finished simple implementation of stack with Node class.

--- DataStructures/stack/Quickstart.java
+++ DataStructures/stack/Quickstart.java
@@ -1,83 +1,32 @@
-import java.util.Stack;
-import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Quickstart {
 
-    public static final Pattern UNSIGNED_DOUBLE = Pattern.compile("((\\d+\\.?\\d*)|(\\.\\d+))([Ee][-+]?\\d+)?.*?");
-    public static final Pattern CHARACTER = Pattern.compile("\\S.*?");
-
-    public static void main(String[] args) {
-        System.out.println(evaluate("((5+2)*(3+9))"));
-    } 
-
-    /*Practicing with imported Stack*/
-    public static double evaluate(String expression) {
-        Stack<Double> numbers = new Stack<Double>();
-        Stack<Character> operations = new Stack<Character>();
-
-        //Converting expression to a Scanner for easier processing.
-        Scanner input = new Scanner(expression);
-        //next string holds the next piece of the expression.
-        String next;
-        
-        while(input.hasNext()) {
-            if(input.hasNext(UNSIGNED_DOUBLE)) { // Checks if the next character in the expression is a number.
-                next = input.findInLine(UNSIGNED_DOUBLE);
-                numbers.push(Double.valueOf(next));
-            }
-            else {  // If it is not a number than it is an operation or parenthesis. (,),+,-,*, or /
-                next = input.findInLine(CHARACTER);
-                switch(next.charAt(0)) {
-                    case '+': //Addition
-                    case '-': //Subtraction
-                    case '*': //Multiplication
-                    case '/': //Division
-                        operations.push(next.charAt(0));
-                        break;
-                    case ')': // Right paranthesis indicates to evaluate the top two numbers on the stack.
-                        evaluateStackTops(numbers, operations);
-                        break;
-                    case '(': //Left paranthesis
-                        break;
-                    default:
-                        input.close();
-                        throw new IllegalArgumentException("Illegal character");
-                }
-            }
-        }
-        System.out.println(numbers.size());
-        if(numbers.size() != 1) {
-            input.close();
-            throw new IllegalArgumentException("Illegal input expression");
-        }
-        input.close();
-        return numbers.pop();
-    }
-
-    public static void evaluateStackTops(Stack<Double> numbers, Stack<Character> operations) {
-        double operand1, operand2;
-
-        if(numbers.size() < 2 || operations.isEmpty())
-            throw new IllegalArgumentException("Illegal expression");
-        operand2 = numbers.pop();
-        operand1 = numbers.pop();
-
-        switch(operations.pop()) {
-            case '+':
-                numbers.push(operand1 + operand2);
-                break;
-            case '-':
-                numbers.push(operand1 - operand2);
-                break;  
-            case '*':
-                numbers.push(operand1 * operand2);
-                break;
-            case '/':
-                numbers.push(operand1 / operand2);
-                break;
-            default: 
-                throw new IllegalArgumentException("Illegal operation");
-        }
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Double> nums = new Stack<Double>();
+		
+		/*Pushing elements onto the stack.*/
+		nums.push(2.0);
+		nums.push(4.0);
+		nums.push(6.0);
+		nums.push(8.0);
+		nums.push(10.0);
+		
+		Stack<Double> duplicate = nums.clone(); // Cloning stacks
+		int length = nums.size(); //Retrieving size of stack.
+		
+		/*Popping elements off of the stack.*/
+		for(int i = 0; i < length; i++) {
+			System.out.println(nums.pop()); //Popping element off of stack
+		}
+		
+		System.out.println("Next Stack");
+		
+		for(int i = 0; i < length; i++) {
+			System.out.println(duplicate.peek()); //Peeking at element, but not removing.
+		}
+		
+		System.out.println("Size of nums stack: " + nums.size());
+		System.out.println("Size of duplicate stack: " + duplicate.size());
+	}
 }
